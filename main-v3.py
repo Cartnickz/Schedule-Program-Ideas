bcad6d1c016786149083ee4e67f433955bfb8d4e
Zach Cartnick <zachcartnick@gmail.com>
2024-07-18 13:28:25 -0400

    Simulated Annealing
    Also changed:
    + conflict count log - used to figured out how scores are being calculated
    + check for too many overnights
    + fixed bug where expanded crews were not all being iterated through
    + calculated available hours and offered hours
    + increased score for empty shifts
    + increased score for improper hour count
    + fixed logic error for determining if supervisor was on shift

--- main-v3.py
+++ main-v3.py
@@ -4,6 +4,7 @@
 
 import random
 import copy
+import math
 from itertools import combinations
 
 # setup array for schedule and initialize other variables
@@ -29,10 +30,6 @@ for element in crew_choose_three + crew_choose_four:
     elif len(element) == 4:
         expanded_crew_combos += [element[0] + "/" + element[1] + "/" + element[2] + "/" + element[3]]
 
-print(expanded_crew_combos)
-
-
-
 # create coordinates to navigate each shift on the schedule
 all_shifts = []
 for i in range(len(sche)):
@@ -54,9 +51,15 @@ total_hours['MD'] = 48
 total_hours['BS'] = 48
 total_hours['JP'] = 24
 
+all_assigned_hours = 0
+for name in total_hours:
+    all_assigned_hours += total_hours[name]
+
+print("All assigned hours:", all_assigned_hours, "Available hours:", "336-420 hours")
+
+
 # default set each employee days off to two random days - manually adjust below
 days_off = {}
-days_off["SM"] = [3, 6]
 
 def generate_days_off():
     day_range = [0, 1, 2, 3, 4, 5]
@@ -66,6 +69,7 @@ def generate_days_off():
         day_range.remove(day_off)
         if len(day_range) == 0:
             day_range = [0, 1, 2, 3, 4, 5]
+    days_off["SM"] = [3, 6]
 
 
 def main():
@@ -74,18 +78,18 @@ def main():
     best_days_off = {}
     tries = 200
     sche_attempts = 0
-    print(count_conflicts(sche))
+    print(count_conflicts(sche)[0])
     while sche_attempts < tries:
-        if sche_attempts % 10 == 0:
+        if sche_attempts % 5 == 0:
             generate_days_off()
         print("-------------------------------")
         # print(days_off)
-        current_score = least_conflicts(8)
+        current_score, current_report = least_conflicts(10)
         if current_score < best_score:
-            best_sche = sche.copy()
+            best_sche = copy.deepcopy(sche)
             best_score = current_score
+            best_report = current_report
             best_days_off = days_off
-        count_conflicts(sche)
         make_shift_dict(sche)
         view(sche)
         sche_attempts += 1
@@ -94,7 +98,7 @@ def main():
     print("********************BEST FOUND********************")
     print(best_days_off)
     view(best_sche)
-    print("Score:", best_score)
+    print("Score:", best_score, best_report)
     return best_score, best_sche
 
 
@@ -106,57 +110,41 @@ def least_conflicts(repeat):
     shift_options_copy = all_shifts.copy()
     shifts_assessed = 0
     while shifts_assessed < (len(all_shifts) * repeat):
+        temp = 500 * 0.99 ** shifts_assessed
         shift_coords = shift_options_copy[random.randint(0, len(shift_options_copy) - 1)]
         x = shift_coords[0]
         y = shift_coords[1]
-        best_crew = []
+        best_crew = ''
         best_crew_score = 10000
+        # print(best_crew_score, best_crew_report)
         crew_list_copy = []
         if x == 2:
             crew_list_copy = expanded_crew_combos.copy()
         else:
             crew_list_copy = crew_combos.copy()
         if best_crew_score > 0 or test_sche[x][y] == "":
-            crews_assessed = 0
-            while crews_assessed < len(crew_combos):
+            crews_remaining = True
+            while crews_remaining:
                 test_crew = crew_list_copy[random.randint(0, len(crew_list_copy)-1)]
                 test_sche[x][y] = test_crew
-                test_score = count_conflicts(test_sche)
-                if test_score < best_crew_score:
-                    # print("!veryif", shifts_assessed, crews_assessed,
-                    #       "test", test_score, "best_crew", best_crew_score,
-                    #       "best_shift", best_shift_score,
-                    #       test_crew, best_crew, shift, day, sche[shift][day])
+                test_score, test_report = count_conflicts(test_sche)
+                if test_score <= best_crew_score:
                     best_crew = test_crew
                     best_crew_score = test_score
-                # else:
-                    # print("evifnot", shifts_assessed, crews_assessed,
-                    #       "test", test_score, "best_crew", best_crew_score,
-                    #       "best_shift", best_shift_score,
-                    #       test_crew, best_crew, shift, day, sche[shift][day])
                 # now that crew was assessed, remove it from the list
                 crew_list_copy.remove(test_crew)
-                crews_assessed += 1
-        test_shift_score = count_conflicts(test_sche)
-        if (best_crew_score < count_conflicts(sche)) or sche[x][y] == "":
-            # print("apply", shift, day, test_crew, best_crew, best_crew_score, test_shift_score,
-            #       count_conflicts(sche), sche[shift][day], (best_crew_score < count_conflicts(sche)))
+                crews_remaining = len(crew_list_copy) != 0
+        energy = best_crew_score - count_conflicts(sche)[0]
+        if energy == 0:
+            probabilty = .05
+        else:
+            probabilty = math.e**(-energy/temp)
+        random_num = random.random()
+        if (random_num < probabilty) or sche[x][y] == "":
             sche[x][y] = best_crew
             test_sche = copy.deepcopy(sche)
-            # view(test_sche)
-            # print("test_score:", test_score, "best crew score:", best_crew_score,
-            #       "new_test_count:", count_conflicts(test_sche), "new_sche_count:", count_conflicts(sche),
-            #       "test_shift:", test_shift_score, "curr_sche:", count_conflicts(sche))
-            # view(sche)
         else:
-            # print("not apply", shift, day, test_crew, best_crew, best_crew_score, test_shift_score,
-            #       count_conflicts(sche), sche[shift][day], (best_crew_score < count_conflicts(sche)))
-            # view(test_sche)
             test_sche = copy.deepcopy(sche)
-            # print("test_score:", test_score, "best crew score:", best_crew_score,
-            #       "new_test_count:", count_conflicts(test_sche), "new_sche_count:", count_conflicts(sche),
-            #       "test_shift:", test_shift_score, "curr_sche:", count_conflicts(sche))
-            # view(sche)
         # remove shift now that it has been assessed, reset list if now empty
         shift_options_copy.remove(shift_coords)
         if len(shift_options_copy) == 0:
@@ -168,6 +156,7 @@ def least_conflicts(repeat):
 
 # function for counting conflicts
 def count_conflicts(check_sche):
+    count_log = []
     count = 0
     shift_dict = make_shift_dict(check_sche)
 
@@ -182,7 +171,8 @@ def count_conflicts(check_sche):
             if person in temp_hour_count.keys():
                 temp_hour_count[person] += 6
     for person in temp_hour_count:
-        count += abs(temp_hour_count[person] - total_hours[person])
+        count += abs(temp_hour_count[person] - total_hours[person]) * 5
+        count_log += [(person, "hours", abs(temp_hour_count[person] - total_hours[person]))]
 
     # conflict if improper shift lengths (i.e cannot be longer than 18)
     for person in shift_dict:
@@ -200,6 +190,7 @@ def count_conflicts(check_sche):
             for shift_count in shifts_per_day:
                 if shift_count > 2:
                     count += 100
+                    count_log += [(person, "shifts per day")]
             # iterate through each shift in chronological order for each person - track back-to-back shifts and breaks
             for shift in chron_shifts:
                 if shift in person_shifts:
@@ -218,13 +209,17 @@ def count_conflicts(check_sche):
         for item in consec_shifts:
             if item > 3:
                 count += 100
+                count_log += [(person, "24s")]
             elif item == 3:
                 count += 50
+                count_log += [(person, "18s")]
             elif item == 1:
-                count += 25
+                count += 15
+                count_log += [(person, "6s")]
         for item in consec_breaks:
             if item < 2:
                 count += 100
+                count_log += [(person, "6 hour break")]
 
     # conflict if on the overnight and morning after
     for day in range(len(check_sche[0])):
@@ -232,7 +227,8 @@ def count_conflicts(check_sche):
             overnight_crew = check_sche[0][day].split("/")
             for member in overnight_crew:
                 if member in check_sche[1][day].split("/"):
-                    count += 100
+                    count += 120
+                    count_log += [(overnight_crew, "overnight_morn")]
 
     # conflict if working on day off or time off
     for person in shift_dict:
@@ -242,20 +238,36 @@ def count_conflicts(check_sche):
             for shift in person_shifts:
                 if shift[1] in days_off[person]:
                     count += 100
+                    count_log += [(person, "day off", shift[1])]
 
     # prioritize supervisors on day shifts
     day_shifts = sche[1] + sche[2]
     for shift in day_shifts:
         shift_split = shift.split("/")
-        if not(supervisors[0] in shift_split or supervisors[1]) in shift_split:
+        if not(supervisors[0] in shift_split or supervisors[1] in shift_split):
             count += 20
+            count_log += ["supervisor"]
+
+    # no too many overnights
+    person_overnight = {}
+    for person in shift_dict:
+        if person not in person_overnight.keys():
+            person_overnight[person] = 0
+        for shift in shift_dict[person]:
+            if shift[0] == 0:
+                person_overnight[person] += 1
+    for person in person_overnight:
+        if person_overnight[person] > 2:
+            count += 50
+            count_log += [(person, "overnight", person_overnight[person])]
 
     # conflict if empty
-    for shift in sche:
+    for shift in check_sche:
         for day in shift:
             if day == "":
-                count += 100
-    return count
+                count += 200
+                count_log += ["empty"]
+    return count, count_log
 
 
 # function for drawing results
